d0f75c90afeba8519b0ce321fc4c5af4ce881bac
Canttuchdiz <hgoldrich@gmail.com>
2024-08-22 16:31:25 -0400

    Finished first version

--- debate/cogs/debates.py
+++ debate/cogs/debates.py
@@ -1,10 +1,13 @@
 import asyncio
+import datetime
+
 import discord
 from discord import app_commands, Interaction, User, Member, Color, Embed, CategoryChannel
 from discord.ext import commands
 from debate.utils.extentsions import PrismaExt
 from discord.types.snowflake import Snowflake
-from typing import Union
+from collections import Counter
+from typing import Dict, List, Any, Tuple
 import traceback
 
 
@@ -16,6 +19,30 @@ class Debates(commands.Cog):
         self.prisma = PrismaExt()
         self.loop = asyncio.get_event_loop()
         self.loop.create_task(self.prisma.connect_client())
+
+    @staticmethod
+    def rank_records(records: List) -> list[Tuple[str, int]]:
+        rankings = dict()
+        for record in records:
+            winnerID = record.winnerID
+            if not (winnerID in rankings):
+                rankings[winnerID] = 0
+            rankings[winnerID] += 1
+        return Counter(rankings).most_common()
+
+    @app_commands.command(name="leaderboard", description="Ranks best debaters")
+    async def leaderboard(self, interaction: Interaction) -> None:
+        embed = discord.Embed(title="Debate Leaderboard", color=Color.dark_teal(), timestamp=datetime.datetime.utcnow())
+        embed.set_footer(text="\u200b", icon_url=interaction.user.avatar)
+        records = await self.prisma.debate.find_many()
+        rankings = Debates.rank_records(records)
+        formatted_ranks = ""
+        for i, rank in enumerate(rankings):
+            winnerID = rank[0]
+            count = rank[1]
+            formatted_ranks += f"{(i + 1)}. <@{winnerID}> **• {count}** debate(s) won.\n"
+        embed.description = formatted_ranks
+        await interaction.response.send_message(embed=embed)
 
     @app_commands.command(name="debates", description="Lists all logged debates")
     async def debates(self, interaction: Interaction) -> None:
@@ -31,7 +58,8 @@ class Debates(commands.Cog):
         await interaction.response.send_message(embed=embed)
 
     @group.command(name="add", description="Adds debate to records")
-    async def add(self, interaction: Interaction, debater1: Member, debater2: Member, topic: str, winner: Member) -> None:
+    async def add(self, interaction: Interaction, debater1: Member, debater2: Member, topic: str,
+                  winner: Member) -> None:
         await self.prisma.debate.create(
             data={
                 'userID1': debater1.id,
